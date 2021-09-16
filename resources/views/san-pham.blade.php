86e127801040f6ae41069d70424f5fb880b2f7fc
Kien Van Tran <65330689+pipiakrt@users.noreply.github.com>
2021-09-16 16:48:23 +0700

    á fsa

--- resources/views/san-pham.blade.php
+++ resources/views/san-pham.blade.php
@@ -448,10 +448,24 @@
         .cs-gia-tri .service2 {
             margin-top: 0;
             padding-bottom: 0;
+            position: relative;
+        }
+        .cs-gia-tri .service2 .bg-sv2 {
+            position: absolute;
             background-image: url('https://i.ytimg.com/vi/R9mXtzn8meE/maxresdefault.jpg');
             background-repeat: no-repeat;
             background-size: cover;
             background-position: center;
+            left: 0;
+            right: 0;
+            top: 0;
+            bottom: 0;
+            z-index: -1;
+            opacity: 0.7;
+        }
+        .cs-gia-tri .gia-tri-cot-loi .block {
+            margin-top: 50px;
+            margin-bottom: 40px;
         }
         .cs-gia-tri .gia-tri-cot-loi {
             margin-top: 0;
@@ -459,15 +473,77 @@
             min-height: unset;
             border-radius: 55px 0px;
             background-color: unset;
+            padding: 0;
         }
         .cs-gia-tri .row.g-0.px-0.px-lg-5 {
             background: white;
-            border-radius: 55px 0;
+            border-radius: 55px;
             margin-top: 50px;
         }
     </style>
     <div class="cs-gia-tri">
-        <x-gia-tri />
+        <div class="service2 ketqua-service" id="san-pham">
+            <div class="bg-sv2"></div>
+            <div class="container">
+                <div class="gia-tri-cot-loi bds-lg-initial">
+                    <div class="titlecotloi text-dark text-center">
+                        <h2 class="title-h1"><span class="h1">10</span> GIÁ TRỊ CHỈ CÓ TẠI</h2>
+                        <h2 class="title-h1">BẢN ĐỒ CUỘC ĐỜI THẦN SỐ HỌC SALA</h2>
+                    </div>
+                    <div class="row g-0 px-0 px-lg-5">
+                        <div class="col-lg-6">
+                            <div class="block">
+                                <div class="d-flex">
+                                    <img height="34" width="34" src="/img/icon/list.png" alt="Thần số học">
+                                    <div class="w-75"><span class="DGESGH2_1">Cung cấp đầy đủ 19 chỉ số quan trọng toàn diện nhất dành cho bạn.</span></div>
+                                </div>
+                                <div class="d-flex">
+                                    <img height="34" width="34" src="/img/icon/list.png" alt="Thần số học">
+                                    <div class="w-75"><span class="DGESGH2_1">Góc nhìn tích cực, xác định bản chất, tư vấn theo quan điểm xây dựng và phát triển bản thân.</span></div>
+                                </div>
+                                <div class="d-flex">
+                                    <img height="34" width="34" src="/img/icon/list.png" alt="Thần số học">
+                                    <div class="w-75"><span class="DGESGH2_1">Cho bạn lời khuyên 3 năm sắp tới để đạt được đỉnh thành công.</span></div>
+                                </div>
+                                <div class="d-flex">
+                                    <img height="34" width="34" src="/img/icon/list.png" alt="Thần số học">
+                                    <div class="w-75"><span class="DGESGH2_1">Hiểu sâu sắc bản thân, nhận biết điểm mạnh, điểm yếu để phát triển sức mạnh, năng lực nội tại bên trong bản thân bạn.</span></div>
+                                </div>
+                                <div class="d-flex">
+                                    <img height="34" width="34" src="/img/icon/list.png" alt="Thần số học">
+                                    <div class="w-75"><span class="DGESGH2_1">Nhận thức năng lực thiên bẩm của bản thân làm chỗ dựa cho đam mê để lựa chọn công việc phù hợp với bạn.</span></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="block">
+                                <div class="d-flex justify-content-lg-center">
+                                    <img height="34" width="34" src="/img/icon/list.png" alt="Thần số học">
+                                    <div class="w-75"><span class="DGESGH2_1">Thấu hiểu con cái, vợ chồng của mình để đạt được hạnh phúc gia đình.</span></div>
+                                </div>
+                                <div class="d-flex justify-content-lg-center">
+                                    <img height="34" width="34" src="/img/icon/list.png" alt="Thần số học">
+                                    <div class="w-75"><span class="DGESGH2_1">Được chuyên gia hỗ trợ luận giải và định hướng giải pháp, phát triển đột phá. </span></div>
+                                </div>
+                                <div class="d-flex justify-content-lg-center">
+                                    <img height="34" width="34" src="/img/icon/list.png" alt="Thần số học">
+                                    <div class="w-75"><span class="DGESGH2_1">Hiểu rõ và điều phối nhân sự một cách hiệu quả nhất.</span></div>
+                                </div>
+                                <div class="d-flex justify-content-lg-center">
+                                    <img height="34" width="34" src="/img/icon/list.png" alt="Thần số học">
+                                    <div class="w-75"><span class="DGESGH2_1">Phát triển tối đa năng lực cá nhân, tạo ra giá trị lớn cho bản thân và xã hội.</span></div>
+                                </div>
+                                <div class="d-flex justify-content-lg-center">
+                                    <img height="34" width="34" src="/img/icon/list.png" alt="Thần số học">
+                                    <div class="w-75"><span class="DGESGH2_1">Có được điểm tựa để đi đúng định hướng bạn thân.</span></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="py-3"></div>
+        </div>
     </div>
     <div class="dangkytth text-white py-5 mb-3 pb-3" id="cacbuocdk">
         <div class="container">
