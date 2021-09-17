e169ec9dd41fc19f3628504bc83659ce9fd182bf
Kien Van Tran <65330689+pipiakrt@users.noreply.github.com>
2021-09-17 08:28:00 +0700

    fsad

--- resources/views/components/post-item.blade.php
+++ resources/views/components/post-item.blade.php
@@ -8,15 +8,11 @@
         <div class="col-md-8 col-lg-7 d-flex align-items-center">
             <div class="post-preview mt-4 mt-md-0">
                 <div class="post-name">
-                    <a href="/tin-tuc/{{ $item->slug }}"><h2>{{ $item->name }}</h2></a>
+                    <a href="/{{ request()->segment(2) }}/{{ $item->slug }}"><h2>{{ $item->name }}</h2></a>
                 </div>
                 <div class="post-desc"><span>{{ $item->description }}</span></div>
                 <div class="submit-post">
-                    @if ($item->type == "post")
-                        <a href="/tin-tuc/{{ $item->slug }}">Xem thêm</a>
-                    @elseif ($item->type == "qa")
-                        <a href="/hoi-dap/{{ $item->slug }}">Xem thêm</a>
-                    @endif
+                    <a href="/{{ request()->segment(2) }}/{{ $item->slug }}">Xem thêm</a>
                 </div>
             </div>
         </div>
