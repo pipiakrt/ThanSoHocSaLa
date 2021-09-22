63c994d9ce793047c83c9ebdce74106995b70a16
Kien Van Tran <65330689+pipiakrt@users.noreply.github.com>
2021-09-22 16:15:37 +0700

    fasdfasf

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        'App\Console\Commands\BackupDatabase',
     ];
 
     /**
@@ -24,6 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->command('BackupDatabase')->hourly();
         // $schedule->command('inspire')->hourly();
     }
 
