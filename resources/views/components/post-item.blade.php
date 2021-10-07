1c423f33b54ba31a1bf7ac98217b6dd4b364425f
Kien Van Tran <65330689+pipiakrt@users.noreply.github.com>
2021-10-07 18:52:26 +0700

    fsda

--- resources/views/components/post-item.blade.php
+++ resources/views/components/post-item.blade.php
@@ -8,11 +8,11 @@
         <div class="col-md-8 col-lg-7 d-flex align-items-center">
             <div class="post-preview mt-4 mt-md-0">
                 <div class="post-name">
-                    <a href="/{{ request()->segment(2) }}/{{ $item->slug }}"><h2>{{ $item->name }}</h2></a>
+                    <a href="{{ $item->slug }}"><h2>{{ $item->name }}</h2></a>
                 </div>
                 <div class="post-desc"><span>{{ $item->description }}</span></div>
                 <div class="submit-post">
-                    <a href="/{{ request()->segment(2) }}/{{ $item->slug }}">Xem thêm</a>
+                    <a href="{{ $item->slug }}">Xem thêm</a>
                 </div>
             </div>
         </div>
